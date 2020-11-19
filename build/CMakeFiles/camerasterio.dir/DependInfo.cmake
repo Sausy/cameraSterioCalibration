e5d0c38f98f7edeeb9c555a2bdf2fcc1e3bbdf30
Sausy <Michael.Sausmikat@gmx.at>
2020-11-19 14:18:00 +0100

    wsl2 build

--- build/CMakeFiles/camerasterio.dir/DependInfo.cmake
+++ build/CMakeFiles/camerasterio.dir/DependInfo.cmake
@@ -4,38 +4,38 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/include/load3dModel/3dModel.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/load3dModel/3dModel.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/include/usbDriver/driver_htcDongle.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/usbDriver/driver_htcDongle.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/include/usbDriver/interface_htcDongle.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/usbDriver/interface_htcDongle.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/include/usbDriver/parser_htcDongle.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/usbDriver/parser_htcDongle.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/include/usbDriver/rayFinder.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/usbDriver/rayFinder.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/include/utils/DataMatcher.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/utils/DataMatcher.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/include/utils/Utils.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/utils/Utils.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/include/utils/testData.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/utils/testData.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/main.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/main.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/src/CsvReader.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/CsvReader.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/src/CsvWriter.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/CsvWriter.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/src/Mesh.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/Mesh.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/src/ModelRegistration.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/ModelRegistration.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/src/PnPProblem.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/PnPProblem.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/src/RobustMatcher.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/RobustMatcher.cpp.o"
-  "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/src/Utils_openCV.cpp" "/mnt/c/Users/Micha/Projekt/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/Utils_openCV.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/include/load3dModel/3dModel.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/load3dModel/3dModel.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/include/usbDriver/driver_htcDongle.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/usbDriver/driver_htcDongle.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/include/usbDriver/interface_htcDongle.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/usbDriver/interface_htcDongle.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/include/usbDriver/parser_htcDongle.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/usbDriver/parser_htcDongle.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/include/usbDriver/rayFinder.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/usbDriver/rayFinder.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/include/utils/DataMatcher.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/utils/DataMatcher.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/include/utils/Utils.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/utils/Utils.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/include/utils/testData.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/include/utils/testData.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/main.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/main.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/src/CsvReader.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/CsvReader.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/src/CsvWriter.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/CsvWriter.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/src/Mesh.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/Mesh.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/src/ModelRegistration.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/ModelRegistration.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/src/PnPProblem.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/PnPProblem.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/src/RobustMatcher.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/RobustMatcher.cpp.o"
+  "/home/sausy/Projects/lighthouse/cameraSterioCalibration/src/Utils_openCV.cpp" "/home/sausy/Projects/lighthouse/cameraSterioCalibration/build/CMakeFiles/camerasterio.dir/src/Utils_openCV.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_CXX
-  "SEARCH_PATH=\"/mnt/c/Users/Micha/Projekt/cameraSterioCalibration\""
+  "SEARCH_PATH=\"/home/sausy/Projects/lighthouse/cameraSterioCalibration\""
   )
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include/opencv4"
   "../src"
   "../include"
   "../include/usbDriver"
   "../include/load3dModel"
   "../include/utils"
+  "/usr/local/include/opencv4"
   )
 
 # Targets to which this target links.
--- include/utils/DataMatcher.cpp
+++ include/utils/DataMatcher.cpp
@@ -9,40 +9,52 @@
 
 #include "Utils.hpp"
 
-DataMatcher::DataMatcher(double * params_Lighthouse_){
+DataMatcher::DataMatcher(double * params_Lighthouse_):\
+                        BaseStationsEventCount{0},\
+                        availableBaseStations{false}
+{
     for (size_t i = 0; i < 4; i++) {
       params_Lighthouse[i] = *params_Lighthouse_;
       params_Lighthouse_++;
     }
+    goodCount = 0;
 
 }
 
 //if we get a certain amount of data from a base Station
 //it will be added to the list of known Base Stations
 void DataMatcher::registNewBaseStation(const std::vector<int> channel){
+
   for (size_t i = 0; i < channel.size(); i++) {
-    //if we get enough  base events, the base will be added to the available list
-    if(BaseStationsEventCount[channel[i]] < MIN_POSITIV_BASE_EVENTS){
-        BaseStationsEventCount[channel[i]]++;
-    }else{
-      availableBaseStations[channel[i]] = true;
-    }
-  }
-
-
-  //deregister base if it doesn't ocure in a while
-  for (size_t j = 0; j < MAX_BASE_AMOUNT; j++) {
-    //check if base was already registerd
-    if(availableBaseStations[channel[j]]){
-      std::cout<<"\nBase Event Counter: " << BaseStationsEventCount[channel[j]];
-      if(BaseStationsEventCount[channel[j]] > 10){
-          BaseStationsEventCount[channel[j]]--;
+    if(channel[i] < MAX_BASE_AMOUNT){
+      //if we get enough  base events, the base will be added to the available list
+      if(BaseStationsEventCount[channel[i]] < MIN_POSITIV_BASE_EVENTS){
+          BaseStationsEventCount[channel[i]]++;
       }else{
-          //if the amount of base events false under 10 .. it will be degristerd
-          availableBaseStations[channel[j]] = false;
+        availableBaseStations[channel[i]] = true;
       }
     }
   }
+
+
+
+  //deregister base if it doesn't ocure in a while
+  for (size_t j = 0; j < channel.size(); j++) {
+    if(channel[j] < MAX_BASE_AMOUNT){
+    //check if base was already registerd
+      if(availableBaseStations[channel[j]]){
+        //std::cout<<"\nBase Event Counter: " << BaseStationsEventCount[channel[j]];
+        if(BaseStationsEventCount[channel[j]] > 10){
+            BaseStationsEventCount[channel[j]]--;
+        }else{
+            //if the amount of base events false under 10 .. it will be degristerd
+            availableBaseStations[channel[j]] = false;
+        }
+
+      }
+    }
+  }
+
 }
 
 
@@ -72,10 +84,8 @@ bool DataMatcher::matchData(const std::vector<std::vector<float>> inVec,\
   //std::vector<std::vector<double>> * vbuf = &sensorData_buffer;
   //std::vector<std::vector<double>> * v2d = &sensorData_solver_2d;
   //std::vector<std::vector<double>> * v4d = &sensorData_solver_3d;
-  std::cout << "\nMatch Data: size=" << id.size();
+  //std::cout << "\nMatch Data: size=" << id.size();
 
-  bool idIsTaken[64] = {0};
-  uint8_t goodCount = 0;
 
   for(size_t i = 0; i < id.size(); i++){
     //std::cout << "\nMatch Data: size=" << id.size() << "/i: " << i;
@@ -83,14 +93,16 @@ bool DataMatcher::matchData(const std::vector<std::vector<float>> inVec,\
     //check if base station id is one of the registered ones
     if(availableBaseStations[channel[i]]){
       //TODO: this should be some kind of kalman filter setup ...
+      //std::cout<<"\nGOOD COUnT: " << goodCount;
       if(idIsTaken[id[i]]){
       //if(false){
-          std::cout<<"\nID: " << id[i] << " was already taken";
+          //std::cout<<"\nID: " << id[i] << " was already taken";
+          ;//
       }else{
           //std::cout<<"\nID: " << id[i] << " ac: " << azimuth[i];
           //TODO: DIRTY WORKAROUND FOR THE TEST DATA
           //NEEDS Prober filtering
-          if(elevation[i] < 20.0){
+          if(elevation[i] < (20.0 * M_PI/180.0)){
               //sensorData_solver_2d.push_back(std::vector<float>(0,0));
               //sensorData_solver_3d.push_back(std::vector<float>(0,0));
 
@@ -101,6 +113,8 @@ bool DataMatcher::matchData(const std::vector<std::vector<float>> inVec,\
               //load the 2d and 3d to a vector
               cv::Point2f buf2d;
               cv::Point3f buf3d;
+
+              //int id_ = id.push_back();
 
               buf2d.x = (float)Vec2D[0];
               buf2d.y = (float)Vec2D[1];
@@ -128,10 +142,10 @@ bool DataMatcher::matchData(const std::vector<std::vector<float>> inVec,\
 
 
 
-
-
-    if(goodCount > 20){
-      break;
+    if(goodCount >= 7){
+      goodCount = 7;
+      return true;
+      //break;
     }
   }
 
